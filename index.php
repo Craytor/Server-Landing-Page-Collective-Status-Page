dce562cc3bcee161942f3e759f333608a2d9ed77
Tyler Youschak <tyleryouschak@gmail.com>
2014-07-25 01:04:25 -0400

    Added the main server status page with data

--- index.php
+++ index.php
@@ -1,36 +1,59 @@
 <?php
+    session_start();
     $sites = array('acadia.team-radiant.com', 'bamboo.team-radiant.com', 'cosmos.team-radiant.com', 'droplet.team-radiant.com');
+
+    foreach($sites as $site) {
+        $_SESSION[$site] = json_decode(file_get_contents("http://" . $site . "/?json=1"));
+    }
 
 
 ?>
 <html>
     <head>
         <title>Status</title>
-        <link rel="stylesheet" href="main.css" />
+        <link rel="stylesheet" href="css/main.css" />
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="js/jqknob.js"></script>
+        <script>
+            $(document).ready(function() {
+                // Show ring charts
+                $("#k-disk, #k-memory, #k-swap, #k-cpu").knob({
+                    readOnly: true,
+                    width: 40,
+                    height: 40,
+                    thickness: 0.2,
+                    fontWeight: 'normal',
+                    bgColor: 'rgba(127,127,127,0.15)', // 50% grey with a low opacity, should work with most backgrounds
+                    fgColor: '#ccc'
+                });
+                // Start AJAX update loop
+                update();
+            });
+        </script>
     </head>
     <body>
         <section class="header">
             <h1>Server Status Page</h1>
         </section>
         <section class="list">
-            <div class="status" style="background-image: url('http://acadia.team-radiant.com/img/bkg.jpg'); background-size: 100%; background-position: center center;" >
+            <?php foreach($sites as $site) { ?>
+
+            <div class="status" style="background-image: url('<?php echo $_SESSION[$site]->{'img'}; ?>'); background-size: 100%; background-position: center center;" >
                 <div class="left">
-                    <h4>acadia.team-radiant.com</h4>
-                    <span>162.243.51.246</span>
+                    <h4><?php echo $site; ?></h4>
+                    <span><?php echo $_SESSION[$site]->{'ip'}; ?></span>
                 </div>
-                <div clas="right">
-
+                <div class="right">
+                    Uptime: <span id="uptime"><?php echo $_SESSION[$site]->{'uptime'}; ?></span>&emsp;
+                    Disk usage: <input id="k-disk" value="<?php echo $_SESSION[$site]->{'disk'}; ?>">&emsp;
+                    Memory: <input id="k-memory" value="<?php echo $_SESSION[$site]->{'memory'}; ?>">&emsp;
+                    <?php if($_SESSION[$site]->{'swap_total'} !== "0") { ?>
+                        Swap: <input id="k-swap" value="<?php echo $_SESSION[$site]->{'swap'}; ?>">&emsp;
+                    <?php } ?>
                 </div>
             </div>
-            <div class="status" style="background-image: url('http://bamboo.team-radiant.com/img/bkg.jpg'); background-size: 100%; background-position: center center;">
 
-            </div>
-            <div class="status" style="background-image: url('http://cosmos.team-radiant.com/img/bkg.jpeg'); background-size: 100%; background-position: center center;">
-
-            </div>
-            <div class="status" style="background-image: url('http://droplet.team-radiant.com/img/bkg.jpg'); background-size: 100%; background-position: center center;">
-
-            </div>
+            <?php } ?>
 
 
         </section>
