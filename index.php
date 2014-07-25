c714e01c90afbc2e08a9eb63502dadd4dfc36ad9
Tyler Youschak <tyleryouschak@gmail.com>
2014-07-24 21:15:05 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    $sites = array('acadia.team-radiant.com', 'bamboo.team-radiant.com', 'cosmos.team-radiant.com', 'droplet.team-radiant.com');
+
+
+?>
+<html>
+    <head>
+        <title>Status</title>
+        <link rel="stylesheet" href="main.css" />
+    </head>
+    <body>
+        <section class="header">
+            <h1>Server Status Page</h1>
+        </section>
+        <section class="list">
+            <div class="status" style="background-image: url('http://acadia.team-radiant.com/img/bkg.jpg'); background-size: 100%; background-position: center center;" >
+                <div class="left">
+                    <h4>acadia.team-radiant.com</h4>
+                    <span>162.243.51.246</span>
+                </div>
+                <div clas="right">
+
+                </div>
+            </div>
+            <div class="status" style="background-image: url('http://bamboo.team-radiant.com/img/bkg.jpg'); background-size: 100%; background-position: center center;">
+
+            </div>
+            <div class="status" style="background-image: url('http://cosmos.team-radiant.com/img/bkg.jpeg'); background-size: 100%; background-position: center center;">
+
+            </div>
+            <div class="status" style="background-image: url('http://droplet.team-radiant.com/img/bkg.jpg'); background-size: 100%; background-position: center center;">
+
+            </div>
+
+
+        </section>
+    </body>
+</html>
